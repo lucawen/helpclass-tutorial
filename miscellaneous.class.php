e3d82de563cb72319a25b495fbf5a27f28cbd4ac
Aloisio1Augusto2 <1337fungamer@gmail.com>
2015-08-05 20:01:30 -0300

    Update miscellaneous.class.php

--- miscellaneous.class.php
+++ miscellaneous.class.php
@@ -23,3 +23,16 @@ class Miscellaneous {
 	}
 
 }
+//Autor:Aloisio e Augusto turma 222n
+public function numerosprimos ($numero){
+<?php
+//exemplo com o numero 10
+$numero = 10;
+
+if (gmp_prob_prime($numero)) {
+	print 'É primo';
+} else {
+	print 'Não é primo';
+}
+
+?>

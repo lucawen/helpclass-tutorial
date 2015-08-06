800fe4176130982933bb52ad5e4fc5c5f1ddec8b
IanEHumberto <IanEHumberto@users.noreply.github.com>
2015-08-05 21:12:09 -0300

    Update miscellaneous.class.php

--- miscellaneous.class.php
+++ miscellaneous.class.php
@@ -219,4 +219,22 @@ if ($_POST){
 ?>
 
 	}
+	/** Ian e Humberto, Próximo Número Primo
+	*/
+	<?php
+    $max = 2147483647;
+    
+    $primesFound = 0;
+    $probablePrimes = 0;
+
+    for ($x = 1; $x <= $max; $x++) {
+        $primeStatus = gmp_prob_prime($x);
+        if ($primeStatus == 1) {
+            $probablePrimes++;
+        } else if ($primeStatus == 2) {
+            $primesFound++;
+        }
+    }
+    echo "Total de primos encontrados: " . $primesFound . " entre 1 e " . $max . ". Primos prováveis nesse intervalo: " . $probablePrimes;
+?>
 ?>
